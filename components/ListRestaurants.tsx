ce006e0bfabc349203d1ea4163c6d13958d2544f
hieple <hieplevuc@gmail.com>
2024-06-22 17:41:34 +0700

    feat: Setup loading component

--- components/ListRestaurants.tsx
+++ components/ListRestaurants.tsx
@@ -2,9 +2,11 @@
 import React from "react";
 import { trpc } from "../utils/trpc";
 import { RestaurantCard } from "./RestaurantCard";
+import Loading from "@/app/loading";
 
 const ListRestaurants = () => {
-  const { data: resp } = trpc.getRestaurants.useQuery();
+  const { data: resp, isLoading } = trpc.getRestaurants.useQuery();
+  if (isLoading) return <Loading></Loading>;
   return (
     <div className="flex flex-col md:flex-row flex-wrap gap-6 justify-center items-start">
       {resp?.data.restaurants.length === 0 ? (
