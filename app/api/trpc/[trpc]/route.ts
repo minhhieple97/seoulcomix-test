0019e62a6db66f5b996ad5ca56caf5efe347daa0
hieple <hieplevuc@gmail.com>
2024-06-24 13:31:09 +0700

    feat: Setup cors

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -3,9 +3,21 @@ import {
   fetchRequestHandler,
 } from "@trpc/server/adapters/fetch";
 import { appRouter } from "../trpc-router";
+import { NextResponse } from "next/server";
 
-const handler = (request: Request) => {
+const handler = (request: Request,response:Response) => {
   console.log(`incoming request ${request.url}`);
+  if (request.method === 'OPTIONS') { 
+    return new NextResponse("Cors Verified", {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Credentials": "true",
+      },
+      status:200
+    })
+  }
   return fetchRequestHandler({
     endpoint: "/api/trpc",
     req: request,
--- utils/trpc-provider.tsx
+++ utils/trpc-provider.tsx
@@ -1,19 +1,19 @@
-'use client';
+"use client";
 
-import { QueryClientProvider } from '@tanstack/react-query';
-import { httpBatchLink, getFetch, loggerLink } from '@trpc/client';
-import { useState } from 'react';
-import superjson from 'superjson';
-import { trpc } from './trpc';
-import queryClient from './query-client';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink, getFetch, loggerLink } from "@trpc/client";
+import { useState } from "react";
+import superjson from "superjson";
+import { trpc } from "./trpc";
+import queryClient from "./query-client";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const TrpcProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const url = process.env.NEXT_PUBLIC_VERCEL_URL
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    : 'http://localhost:3000/api/trpc/';
+    : "http://localhost:3000/api/trpc/";
 
   const [trpcClient] = useState(() =>
     trpc.createClient({
@@ -27,13 +27,19 @@ export const TrpcProvider: React.FC<{ children: React.ReactNode }> = ({
             const fetch = getFetch();
             return fetch(input, {
               ...init,
-              credentials: 'include',
+              credentials: "include",
+              headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "*",
+                "Access-Control-Allow-Headers": "*",
+                "Access-Control-Allow-Credentials": "true",
+              },
             });
           },
         }),
       ],
       transformer: superjson,
-    })
+    }),
   );
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
