74d67ff751f109ad77997cb3af4950405a4f69ac
hieple <hieplevuc@gmail.com>
2024-06-26 10:36:12 +0700

    refactor: Remove unuse var

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -5,19 +5,8 @@ import {
 import { appRouter } from "../trpc-router";
 import { NextResponse } from "next/server";
 
-const handler = (request: Request,response:Response) => {
+const handler = (request: Request) => {
   console.log(`incoming request ${request.url}`);
-  if (request.method === 'OPTIONS') { 
-    return new NextResponse("Cors Verified", {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "*",
-        "Access-Control-Allow-Headers": "*",
-        "Access-Control-Allow-Credentials": "true",
-      },
-      status:200
-    })
-  }
   return fetchRequestHandler({
     endpoint: "/api/trpc",
     req: request,
