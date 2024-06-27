62317f07db02b63dfb3a0497b0cc439b1846b07b
hieple <hieplevuc@gmail.com>
2024-06-27 10:13:55 +0700

    feat: Move global.css to _styles folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { TrpcProvider } from "@/utils/trpc-provider";
-
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { TrpcProvider } from '@/utils/trpc-provider';
+import '@/app/_styles/globals.css';
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Restaurants",
+  title: 'Restaurants',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
